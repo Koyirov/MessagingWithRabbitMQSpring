a8796b2ee66b038dddd75f8af08403ca3abf74fa
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-13 15:11:08 +0200

    initial commit

---
+++ src/test/java/com/example/messagingwithrabbitmqspring/MessagingWithRabbitMQSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingwithrabbitmqspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingWithRabbitMQSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
